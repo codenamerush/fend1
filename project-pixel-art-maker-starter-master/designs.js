d4abf49d29a4d105753b3b98a0d7e42c0171e42e
Aarushi <aarushisharda@gmail.com>
2018-02-25 13:22:49 +0530

    Pixel art maker completed

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -2,9 +2,34 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var table = document.getElementById('pixelCanvas');
+var tableRow = document.getElementById('inputHeight');
+var tableColumn = document.getElementById('inputWeight');
+var color = document.getElementById('colorPicker');
+
+function colorMe(e) {
+	e.target.style.background = color.value;
+}
 
 function makeGrid() {
-
-// Your code goes here!
-
+	var rows = tableRow.value;
+	var cols = tableColumn.value;
+	if((rows < 1) || (cols < 1)) {
+		alert('You need to enter values larger than 0');
+	}
+	else {
+		table.innerHTML = '';
+		for(var i=0; i<rows; i++)
+			{
+				var row = document.createElement('tr');
+				for(var j=0; j<cols; j++)
+				{
+					var column = document.createElement('td');
+					column.addEventListener('click', colorMe);
+					row.append(column);
+				}
+				table.append(row);
+			}
+	}
+	return false;
 }
