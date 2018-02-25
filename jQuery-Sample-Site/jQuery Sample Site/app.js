069b4369549ad6acd5e14c5dcf58bf185d409b90
Aarushi <aarushisharda@gmail.com>
2018-02-25 11:58:43 +0530

    fend1 pixel art maker

--- jQuery-Sample-Site/jQuery Sample Site/app.js
+++ jQuery-Sample-Site/jQuery Sample Site/app.js
@@ -1,3 +1,17 @@
 $(function(){
 	$("img").attr("src", "http://placekitten.com/350/150");
 });
+
+var handler = function(name){
+	console.log("Hi " + name);
+}
+
+var obj = {
+	foo: 'bar',
+	aarushi: function(parameter_handler){
+		// handler is a type of function
+		parameter_handler(obj.foo);
+	}
+};
+
+obj.aarushi(handler);
